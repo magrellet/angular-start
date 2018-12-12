34f92ca610cc04ac851d0b09dfa1601a126c6367
Martin <martin.grellet@sovos.com>
2018-12-12 18:36:50 -0300

    02 - Editing first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root', //product management
+  template: `<div>
+    <h1>{{pageTittle}}</h1>
+    <div>My First Component</div>
+  </div>`
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTittle: string = 'Acme Product Management';
 }
